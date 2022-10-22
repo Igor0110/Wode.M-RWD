90bd5b85e94ae703564877dd7de554bf8b7c14c8
igor <igor@gmail.com>
2022-10-22 17:44:00 +0800

    fix product.html RWD add guide.html css

--- JavaScript.js
+++ JavaScript.js
@@ -17,4 +17,16 @@ window.addEventListener("load",function(){
 
 
 
+    // <!-- Initialize Swiper -->
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 })
