00698da59ff803743ed1a0fbe58525a4583e9797
igor <igor@gmail.com>
2022-10-26 14:51:49 +0800

    add product-page

--- JavaScript.js
+++ JavaScript.js
@@ -2,18 +2,21 @@ window.addEventListener("load",function(){
 
     // 淡入視口
     let oth = $(window).innerHeight();
-    $(window).scroll(function(){
-        let sct = $(this).scrollTop();
-        let all = sct+oth;
-        $(".slideIn").each(function(){
+    $(function(){
+        $(window).scroll(function(){
+          let sct = $(this).scrollTop();
+          let all = sct+oth;
+          $(".slideIn").each(function(){
             let sld = $(this).offset().top;
             if(all >sld){
                 $(this).addClass("active");
             }else{
                 $(this).removeClass("active");
             }
+          });
         });
-    });
+    })
+
 
 
 
@@ -49,6 +52,6 @@ window.addEventListener("load",function(){
     })
     
 
-})
+  })
 
 })
